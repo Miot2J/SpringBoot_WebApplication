6680df9408614b4f0e7996e7eb80b46427f811d9
Miot2J <ck96117@gmail.com>
2021-01-05 23:38:05 +0900

    Make Member Service Test

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,24 +1,65 @@
 package jpabook.jpashop.service;
 
-import org.junit.Test;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
 public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
     @Test
     public void 회원가입() throws Exception {
         //given
-        
+        Member member = new Member();
+        member.setName("cha");
+
         //when
-        
+        Long savedId = memberService.join(member);
+
         //then
+        em.flush(); //persist(영속성 객체를 디비에 강제로 커밋한다(터미널에 로그찍힘)  그 후 @Transactional로인해 롤백)
+        assertEquals(member,memberRepository.findOne(savedId));
     }
+
     @Test
     public void 중복_회원_예외() throws Exception {
         //given
+        Member member1 = new Member();
+        member1.setName("cha1");
+
+        Member member2 = new Member();
+        member2.setName("cha1");
 
         //when
-
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+        }catch (IllegalStateException e){
+            return;
+        }
         //then
+//        fail("예외가 발생 해야 한다.");
+        IllegalStateException thrown = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertEquals("이미 존재하는 회원입니다.", thrown.getMessage());
     }
 }
